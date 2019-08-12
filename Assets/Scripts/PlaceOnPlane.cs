d7f5d49461a6051dd078613d6b35a65850f43221
hgsujay <hgsujay@gmail.com>
2019-08-12 13:04:35 +0530

    pushing version one

---
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+/// <summary>
+/// Listens for touch events and performs an AR raycast from the screen touch point.
+/// AR raycasts will only hit detected trackables like feature points and planes.
+///
+/// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
+/// and moved to the hit position.
+/// </summary>
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    public GameObject m_PlacedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            var mousePosition = Input.mousePosition;
+            touchPosition = new Vector2(mousePosition.x, mousePosition.y);
+            return true;
+        }
+#else
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+#endif
+
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (!IsPointerOverUIObject() && m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+
+        foreach (RaycastResult r in results)
+        {
+            if (r.gameObject.layer == 5)
+            {
+                Debug.Log("On UI");
+                return true;
+            }
+            //return results.Count > 0;
+        }
+        return false;
+    }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    ARRaycastManager m_RaycastManager;
+}
---
+++ Assets/scaleTheCube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scaleTheCube : MonoBehaviour
+{
+    public PlaceOnPlane placeOnPlane;
+    public Camera ARcamera;
+    public Text scaleText;
+    public Text rotationText;
+    public Text fovText;
+    public GameObject sliders;
+    public void scaleCube(Slider slider)
+    {
+        float value = slider.value;
+        placeOnPlane.spawnedObject.transform.localScale = new Vector3(value, value, value);
+        scaleText.text = "SCALE: " + value;
+    }
+    public void rotateCube(Slider slider)
+    {
+        float value = slider.value;
+        placeOnPlane.spawnedObject.transform.rotation = Quaternion.AngleAxis(value, Vector3.up);
+        rotationText.text = "Rotate: " + value;
+    }
+
+    public void changeFOV(Slider slider)
+    {
+        float value = slider.value;
+        ARcamera.fieldOfView = value;
+        fovText.text = "FOV: " + value;
+    }
+
+    public void toggleSliders()
+    {
+        sliders.SetActive(!sliders.activeSelf);
+    }
+}
